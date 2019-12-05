fa29f3650448d34a718087dcbb8848b1d738195c
Mitchell Loeppky <mrl@amazon.com>
2019-12-04 17:29:34 -0800

    Add unit test coverage thresholds.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,14 @@ process.env.PACKAGE_VERSION = 'test.test.test';
 
 module.exports = {
     collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
     roots: ['<rootDir>/src'],
     testMatch: ['**/*.spec.ts'],
     transform: {
