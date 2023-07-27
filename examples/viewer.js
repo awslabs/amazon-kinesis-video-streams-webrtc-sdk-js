1905635329b4db3e0c47fbac62436a805bcf79f1
Yuma Mihira <yuma-m@users.noreply.github.com>
2023-07-27 15:04:26 +0900

    Add callback for self-created data channel

--- examples/viewer.js
+++ examples/viewer.js
@@ -207,7 +207,10 @@ async function startViewer(localView, remoteView, formValues, onStatsReport, onR
         viewer.peerConnection = new RTCPeerConnection(configuration);
         if (formValues.openDataChannel) {
             viewer.dataChannel = viewer.peerConnection.createDataChannel('kvsDataChannel');
+            // Callback for the data channel created by viewer
+            viewer.dataChannel.onmessage = onRemoteDataMessage;
             viewer.peerConnection.ondatachannel = event => {
+                // Callback for the data channel created by master
                 event.channel.onmessage = onRemoteDataMessage;
             };
         }
