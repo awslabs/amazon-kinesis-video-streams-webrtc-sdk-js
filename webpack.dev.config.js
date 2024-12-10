55b27bb4fc887b090facebedb9b4eb6a322ba8fc
sirknightj <jggunawa@amazon.com>
2024-12-09 23:35:54 -0800

    Change the webpack-merge import

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 module.exports = merge.smart(require('./webpack.config'), {
     mode: 'development',
